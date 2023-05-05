7c38ca29b8e7904761357e846e5fb5beaf767945
itlaboj <itlabo.arakawa@gmail.com>
2023-05-05 09:10:27 +0900

    Sample11追加

---
+++ src/p01/Sample11.java
@@ -0,0 +1,11 @@
+package p01;
+
+public class Sample11 {
+
+	public static void main(String[] args) {
+		System.out.println("巨人");
+		System.out.println("阪神");
+		System.out.println("ヤクルト");
+	}
+
+}
